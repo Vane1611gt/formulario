74bc5383e7b9edd7ba227ef64dfe0954862666ab
hellen <hellenyct@mail.com>
2024-07-24 08:15:34 -0600

    diseño principal

---
+++ main.js
@@ -0,0 +1,26 @@
+// Selecciona el elemento con id 'root'
+let root = document.querySelector("#root");
+
+// Crea un nuevo div para el formulario
+let div_formulario = document.createElement("div");
+div_formulario.className = "div-formularios"; // 
+
+// Lista de datos (String)
+let formularios=[
+    { nombre:"Login",link:"https:"},   
+    { nombre:"Login",link:"https:"}, 
+    { nombre:"Login",link:"https:"}, 
+    { nombre:"Login",link:"https:"}, 
+];
+
+// Itera sobre cada elemento de la lista
+formularios.forEach(itemLista => {
+    let div_item = document.createElement("a");
+    div_item.href="itemLista.link";
+    div_item.innerText=itemLista.nombre;
+
+    div_formulario.appendChild(div_item); 
+});
+
+// Agrega el div_formulario a root
+root.appendChild(div_formulario);
